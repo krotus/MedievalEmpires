c1acc799a0a4a8595f5f38f87ead922cb8f778b4
Krotus <andreus.sala@gmail.com>
2016-02-12 02:24:12 +0100

    Another test of gitignore

--- MedievalEmpires/Views/Login.aspx.vb
+++ MedievalEmpires/Views/Login.aspx.vb
@@ -3,6 +3,7 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         MsgBox("Hello World Baby!")
+        MsgBox("Hello World Baby!")
     End Sub
 
 End Class
